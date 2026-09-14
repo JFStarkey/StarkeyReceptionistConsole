e9e5a903f12dc1794ed6140cd5e33a0b1ac9674a
JFStarkey <jay_fluegge@starkey.com>
2026-09-14 10:52:00 -0500

    Add files via upload

--- src/widget-SDK-Voice.js
+++ src/widget-SDK-Voice.js
@@ -28,8 +28,6 @@ template.innerHTML = `
 
     box-sizing:border-box;
 
-    background-color:#003B71;
-
 }
 
 .container::-webkit-scrollbar {
@@ -79,6 +77,14 @@ template.innerHTML = `
 
     background-color: #fff;
 
+    background-image: url("https://jfstarkey.github.io/StarkeyReceptionistConsole/starkey-star.png");
+
+    background-repeat: no-repeat;
+
+    background-position: -20px center;
+
+    background-size: 90%;
+
     cursor: pointer;
 
     transition: .2s;
@@ -116,17 +122,20 @@ template.innerHTML = `
 
     justify-content:center;
 
-    align-items:center;
-
     width:100%;
 
     height:100%;
 
     box-sizing:border-box;
 
-    padding:15px;
+    padding-left:95px;
 
-    position:relative;
+    padding-right:15px;
+	
+	position:relative;
+
+	top:15px;
+
 }
 
 .queueName {
@@ -143,19 +152,9 @@ template.innerHTML = `
 
     line-height:1.2;
 
-    text-align:center;
-
 }
 
-.queueExtension 
-
-{
-
-    position:absolute;
-
-    bottom:12px;
-
-    right:12px;
+.queueExtension {
 
     font-family:
         "Segoe UI",
@@ -166,6 +165,10 @@ template.innerHTML = `
     font-weight:600;
 
     color:#003B71;
+
+    margin-top:4px;
+
+    text-align:right;
 
 }
 
